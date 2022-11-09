5a9717a32a908f1869e4696094f99d5326ff5e6d
INNOMINDSHYD\sdesu <sdesu@innominds.com>
2022-11-09 08:38:11 +0530

    Form Group working

--- emps/src/app/user/user-form-control.component.ts
+++ emps/src/app/user/user-form-control.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormBuilder, FormControl, Validators } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'user-form-control',
@@ -7,12 +7,26 @@ import { FormBuilder, FormControl, Validators } from "@angular/forms";
 })
 export class UserFormControlComponent{
   name: FormControl = this.fb.control('Welcome123', [Validators.required]);
+  loginGroup: FormGroup = this.fb.group({
+    'name': this.name,
+    'password': new FormControl(
+      null,
+      [Validators.required]
+    ),
+    'address': new FormGroup({
+      'street': this.fb.control('Financial Dist', [Validators.required]),
+      'area': this.fb.control('Hyderabad', [Validators.required])
+    })
+  });
   // name: string = 'template model';
   constructor(private fb: FormBuilder){
     this.listenNameChanges();
   }
   updateName(){
     this.name.setValue('Hyderabad');
+  }
+  formSubmitted(){
+    console.log(this.loginGroup.valid, this.loginGroup.value);
   }
   listenNameChanges(){
     this.name.valueChanges.subscribe((data:any)=>{
